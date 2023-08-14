3dab15995ba0a3102b506570731e9797e930759d
pixmin94 <pixmin@outlook.com>
2023-08-14 15:03:11 +0800

    Setup spring boot application

---
+++ backend/src/main/java/com/dive/backend/BackendApplication.java
@@ -0,0 +1,16 @@
+package com.dive.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication { //implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	// @Override
+	// public void run(String... args) throws Exception {}
+
+}
---
+++ backend/src/test/java/com/dive/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dive.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
